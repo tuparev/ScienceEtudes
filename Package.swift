f010697f5df1ca7de7bef9c3e127b333ddc4fbeb
Georg Tuparev <gtupar@tuparev.com>
2023-03-17 05:43:23 +0200

    Adding Date+JulianDay Date Extension written by Hunter

--- Package.swift
+++ Package.swift
@@ -12,18 +12,22 @@ let package = Package(
         .library(name: "UnitsAndMeasurements", targets: ["UnitsAndMeasurements"]),
         .library(name: "CoreMaths", targets: ["CoreMaths"]),
         .library(name: "CoreAstronomy", targets: ["CoreAstronomy"]),
+        .library(name: "AstronomyTime", targets: ["AstronomyTime"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tuparev/SoftwareEtudes", branch: "dev"),
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+
         .target(name: "UnitsAndMeasurements", dependencies: [],            path: "Sources/UnitsAndMeasurements"),
         .target(name: "CoreMaths",            dependencies: [],            path: "Sources/CoreMaths"),
         .target(name: "CoreAstronomy",        dependencies: ["CoreMaths"], path: "Sources/Astronomy/CoreAstronomy"),
+        .target(name: "AstronomyTime",        dependencies: [.product(name: "SoftwareEtudesUtilities", package: "SoftwareEtudes"), "CoreMaths"], path: "Sources/Astronomy/Time"),
 
         .testTarget(name: "UnitsAndMeasurementsTests",dependencies: ["UnitsAndMeasurements"], path: "Tests/UnitsAndMeasurements"),
     ]
---
+++ Sources/Astronomy/Time/Date+JulianDay.swift
@@ -0,0 +1,63 @@
+//
+//  Date+JulianDay.swift
+//  
+//
+//  Created by Hunter William Holland on 17.03.2023.
+//  Copyright © See Framework's LICENSE file
+//
+// Many of the algorithms in this Date extension are based on
+//   Astronomical Algorithms (second edition)
+//   Author: Jean Meeus
+//   Published by Willmann-Bell, Inc. Copyright © 1991, 1998, 2005
+//
+
+import Foundation
+import SoftwareEtudesUtilities
+
+public extension Date {
+
+    init(julianDay: Double) {
+        precondition(julianDay >= 0, "Julian Dates must be positive.")
+
+        let epochSeconds = round((julianDay - 2_440_587.5) * 86400)
+
+        self.init(timeIntervalSince1970: epochSeconds)
+    }
+
+    /// The date's Julian Day (JD), expressed as a decimal number of days since the beginning of the current Julian Period.
+    ///
+    /// The current Julian Period began at noon on the first day of the year -4712 (4713 BCE), making the year 2000 in
+    /// the Julian calendar year 6713 of the current Julian Period.
+    var julianDay: Double {
+        var Y = Double(self.year)
+        var M = Double(self.month)
+        let D = Double(self.day) + (self.fractionalHour / 24)
+
+        if M == 1 || M == 2 { Y -= 1; M += 12 }
+
+        let A = floor(Y / 100)
+        let B = 2 - A + floor(A / 4)
+
+        let JD = floor(365.25*(Y+4716) + floor(30.6001*(M+1))) + D + B - 1524.5
+
+        return JD
+    }
+
+    /// The date's Modified Julian Day (MJD).
+    ///
+    /// The epoch of the MJD system is at midnight on November 17, 1858. That is, the MJD of 1858-11-17 00:00:00 is 0.0.
+    var modifiedJulianDay: Double { self.julianDay - 2_400_000.5 }
+
+}
+
+public extension Date {
+
+    /// The date's year (in the Gregorian calendar) and fraction of progress therethrough.
+    var fractionalYear: Double {
+        let daysInYear   = (self.isInLeapYear) ? 366.0 : 365.0
+        let components   = DateComponents(year: self.year, month: 1, day: 1, hour: 0, minute: 0, second: 0)
+        let januaryFirst = Calendar.gregorianUTC.date(from: components)!
+
+        return Double(self.year) + ((self.julianDay - januaryFirst.julianDay) / daysInYear)
+    }
+}
