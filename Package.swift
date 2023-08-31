70678f13f97813dd4b4bde892eeddf885b8ebd89
Georg Tuparev <gtupar@tuparev.com>
2023-09-01 02:58:12 +0300

    Increasing the minimal versions

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ScienceEtudes",
     defaultLocalization: "en",
-    platforms: [.macOS(.v13), .iOS(.v15), .tvOS(.v11), .watchOS(.v5)],
+    platforms: [.macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Astronomy", targets: ["Astronomy"]),
