00764d453d2dd9b380b835c13b274f6a77a2050a
Georg Tuparev <gtupar@tuparev.com>
2023-10-30 06:28:17 +0200

    Switching to Swift 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         
-        .target(name: "Astronomy",          dependencies: [.product(name: "SoftwareEtudesUtilities", package: "SoftwareEtudes"), "CoreMaths",], path: "Sources/Astronomy"),
+        .target(name: "Astronomy",            dependencies: [.product(name: "SoftwareEtudesUtilities", package: "SoftwareEtudes"), "CoreMaths",], path: "Sources/Astronomy"),
         .target(name: "CoreMaths",            dependencies: [],            path: "Sources/CoreMaths"),
         .target(name: "UnitsAndMeasurements", dependencies: [],            path: "Sources/UnitsAndMeasurements"),
         
